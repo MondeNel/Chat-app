e34e4b061c7ec64afe6edd930c2f9846914fe841
Monde Nel <Mondenel1996@gmail.com>
2025-02-27 13:24:27 +0200

    feat(auth): add checkAuth controller to verify user authentication
    - Return authenticated user details in the response
    - Handle errors and return appropriate status codes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -156,3 +156,12 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
 };
+
+export const checkAuth = (res, req) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error!" });
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -15,5 +15,8 @@ router.post("/logout", logout);
 
 // Update profile route (protected)
 router.put("/update-profile", protectRoute, updateProfile);
+
+// Check if user is authenticated
+router.get("/check", protectRoute, checkAuth)
 
 export default router;
