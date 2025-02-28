d0621c3a0c76f378786f4f506665b69d8b740d81
Monde Nel <Mondenel1996@gmail.com>
2025-02-28 13:05:43 +0200

    feat(user): add route to fetch users for sidebar
    - Define GET /users route protected by protectRoute middleware
    - Add getUsersForSidebar controller to fetch users excluding the authenticated user
    - Export router for use in the main application

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 
 // Load environment variables
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 // Connect to the database and start the server
 connectDB()
---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware"; 
+import { getUsersForSidebar } from "../controllers/user.controller.js"; 
+
+// Create an Express router
+const router = express.Router();
+
+// Define a GET route to fetch users for the sidebar
+// This route is protected by the protectRoute middleware
+router.get("/users", protectRoute, getUsersForSidebar);
+
+// Export the router for use in other parts of the application
+export default router;
