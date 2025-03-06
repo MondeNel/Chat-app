fcaa050f6daa9e5399266645a07bf43fda638a15
Monde Nel <Mondenel1996@gmail.com>
2025-03-06 14:11:30 +0200

    Modified the message route file

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,18 +1,12 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware.js"; 
-import { getUsersForSidebar, sendMessage } from "../controllers/message.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-
-// Create an Express router
 const router = express.Router();
 
-// Define a GET route to fetch users for the sidebar
-// This route is protected by the protectRoute middleware
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
 
-
-// Export the router for use in other parts of the application
 export default router;
