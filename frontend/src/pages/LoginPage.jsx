b261777b08cbc6993602bb26d89ef7e039edcda5
Monde Nel <Mondenel1996@gmail.com>
2025-03-07 15:42:35 +0200

    create some logic for the login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
+import { useAuthStore } from '../store/useAuthStore.js';
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [ formData, setFormData ] = usestate({
+    email: '',
+    password: '',
+  });
+  
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(formData);
+  }
+
+
+
   return (
     <div>LoginPage</div>
   )
