aa253932a5a7aee0d83fa52f7f3c8198df6dca5a
Monde Nel <Mondenel1996@gmail.com>
2025-03-07 22:16:07 +0200

    Add initial ProfilePage component with basic layout and authentication state

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore.js'
 
 const ProfilePage = () => {
+  const { authUser, isUpdatingProfile, updatProfile } = useAuthStore();
+
+  const handleImageUpload = async (e) => {};
+
+
   return (
-    <div>ProfilePage</div>
+    <div className='h-screen pt20'>
+      <div className='max-w-2xl mx-auto p-4 py-8'>
+        <div className='bg-base-300 rounded-xl p-6 space-y-8'>
+          <div className='text-center'>
+            <h1 className='text-2xl font-semi-bold'>Profile</h1>
+            <p className='mt-2'>Your profile information</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
