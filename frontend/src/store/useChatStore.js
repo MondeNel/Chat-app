e83572d380c9f96af71c7d5a052d2679611b45f4
Monde Nel <Mondenel1996@gmail.com>
2025-03-15 12:13:49 +0200

    Add message subscription and unsubscription for real-time updates

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import toast from 'react-hot-toast';
 import { axiosInstance } from '../lib/axios.js';
+import { useAuthStore } from './useAuthStore.js'
 
 /**
  * Zustand store for managing chat-related state.
@@ -60,6 +61,24 @@ export const useChatStore = create((set, get) => ({
             console.error("Failed to send message:", error.response?.data || error);
             toast.error(error.response?.data?.message || "Failed to send message.");
         }
+    },
+
+    subcribeToMessages: () => {
+        const { selectedUser } =  get()
+        if(!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+        socket.on('newMessage', (newMessage) => {
+            set({
+                messages: [...get().message, newMessage],
+            });
+        });
+    },
+
+    unsubscribeFromMessages: () => {
+        const socket = useAuthStore.getState().socket;
+        socket.off('newMessage');
     },
     
 
